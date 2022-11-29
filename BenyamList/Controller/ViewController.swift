43b942639f59b23b5bc96c930efeb3ee72c5d32d
Raiymbek Merekeyev <raiymbekmerekeyev@Raiymbeks-MacBook-Pro.local>
2022-11-29 18:24:25 +0600

    Getting started

---
+++ BenyamList/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BenyamList
+//
+//  Created by Raiymbek Merekeyev on 29.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
